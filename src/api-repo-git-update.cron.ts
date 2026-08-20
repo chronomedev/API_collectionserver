b1ff0e9e6534b61e81ec4f7e9d31bfde541445c8
chronomedev <hansrenee@live.com>
2026-08-21 00:12:21 +0700

    added cron for periodically pull on updated API

---
+++ src/api-repo-git-update.cron.ts
@@ -0,0 +1,21 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { execa } from 'execa';
+
+@Injectable()
+export class APIRepoGitUpdateCron {
+
+  @Cron('* /10 * * * *')
+  async unGitPullPeriodic() {
+
+    const REPO_DIR = '/Users/hanzz/Documents/bruno/hans_API_collection';
+
+    try {
+      const { stdout } = await execa('git', ['-C', REPO_DIR, 'pull', '--ff-only']);
+      console.log(stdout || 'Already up to date');
+      return 'git pull completed';
+    } catch (error) {
+      console.log(error.stderr || error.message);
+    }
+  }
+}
